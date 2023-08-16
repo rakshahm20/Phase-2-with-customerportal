e0f08a7241d2f033a7a2c1b9e47c2eec88d8dd6c
rakshahm20 <96164301+rakshahm20@users.noreply.github.com>
2023-08-16 12:27:44 +0530

    Delete mainapp directory

--- mainapp/pipes/date-format-pipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DateFormatPipePipe } from './date-format-pipe.pipe';
-
-describe('DateFormatPipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new DateFormatPipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- mainapp/pipes/search-table.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SearchTablePipe } from './search-table.pipe';
-
-describe('SearchTablePipe', () => {
-  it('create an instance', () => {
-    const pipe = new SearchTablePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
